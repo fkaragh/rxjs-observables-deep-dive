6424e053626aeaa8e15d042fcd1a2ec5046f7576
fkaragh <fkfurkankara@gmail.com>
2024-09-28 17:56:21 +0300

    observables:
    observables created and also the transition between observables and signals has been made.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, map, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
 
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+
+  customInterval$ = new Observable((subscriber) => {
+    let timesExucuted = 0;
+    const interval = setInterval(()=>{
+      // subscriber.error();
+      if (timesExucuted > 3){
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('emitting new value ...');
+      subscriber.next({message: 'New Value'});
+      timesExucuted++;
+    }, 2000);
+  });
+
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`)
+    })
+  }
+  
+  ngOnInit(){
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val*2)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
+    
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // })
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('completed!'),
+      error: (err)=> console.log('error occured!', err),
+    });
+    const subscription = this.clickCount$.subscribe({
+      next:(val) => console.log(`Clicked button ${this.clickCount()} times.`)
+    })
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount +1);
+  }
 }
